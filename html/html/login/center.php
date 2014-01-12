f7756d6b092a9633789e7dc83eb1427da3116fa8
gsourdrille@gmail.com <gsourdrille@gmail.com@12beb7ba-e8de-a6ef-9cba-bac698d55960>
2014-01-12 23:04:36 +0000

    [HTML] page de login

--- html/html/login/center.php
+++ html/html/login/center.php
@@ -1,18 +1,25 @@
-<div id="center_conteneur">
 	<form action="/myschool/core/controller/login_controller.php" method="post">
+		<div id="connexion_text">
+			Connexion
+		</div>
 		<div id="user_login">
-			<label for="username">Identifiant : </label>
-			<input type="text" name="username" value="<?php if(isset($username)){echo $username;}?>"/>
+			<input class="loginForm" type="text" name="username" value="<?php if(isset($username)){echo $username;}?>" placeholder="Identifiant"/>
 		</div>
 		<div id="pass_login">
-			<label for="password">Mot de passe : </label>
-			<input type="password" name="password" value="<?php if(isset($password)){echo $password;}?>"/>
+			<input class="loginForm" type="password" name="password" value="<?php if(isset($password)){echo $password;}?>" placeholder="Mot de passe"/>
 		</div>
-		<div id="button_login">
-			<input type="submit" value="Connexion">
-		</div>
-		<div id="remember_me">
-			Se souvenir de moi <input type="checkbox">
+		<div class="login_footer">
+			<div id="button_login">
+				<input class="loginButton" type="submit" value="Connexion">
+			</div>
+			<div class="login_tools">
+				<div id="remember_me">
+					Se souvenir de moi <input type="checkbox">
+				</div>
+				<div class="inscription_link">
+					<a href="" >inscription</a>
+				</div>
+			</div>
 		</div>
 		<?php if(isset($error)){?>
 		<div id="error_login">
@@ -20,5 +27,3 @@
 		</div>
 		<?php }?>
 	</form>
-	
-</div>
