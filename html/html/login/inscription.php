6ce160e11a344b971c63e34fac4d8faf4c8a2296
gsourdrille@gmail.com <gsourdrille@gmail.com@12beb7ba-e8de-a6ef-9cba-bac698d55960>
2014-02-16 18:02:23 +0000

    [DEV] amelioration formulaire inscription

--- html/html/login/inscription.php
+++ html/html/login/inscription.php
@@ -3,26 +3,26 @@
 		<div class="connexion_text">
 			Inscription
 		</div>
-		<div class="loginInput">
+		<div class="formulaireInput">
 			<input class="buttonForm" type="text" name="username" value="" placeholder="Adresse email"/>
 		</div>
 		<div id="error_inscription_login" class="error_formulaire" style="display:none"></div>
-		<div class="loginInput">
+		<div class="formulaireInput">
 			<input class="buttonForm" type="password" name="password" value="" placeholder="Mot de passe"/>
 		</div>
 		<div id="error_inscription_mdp" class="error_formulaire" style="display:none"></div>
-		<div class="loginInput">
+		<div class="formulaireInput">
 			<input class="buttonForm" type="password" name="password_bis" value="" placeholder="Répeter mot de passe"/>
 		</div>
-		<div class="loginInput">
+		<div class="formulaireInput">
 			<input class="buttonForm" type="text" name="nom" value="" placeholder="Nom"/>
 		</div>
 		<div id="error_inscription_nom" class="error_formulaire" style="display:none"></div>
-		<div class="loginInput">
+		<div class="formulaireInput">
 			<input class="buttonForm" type="text" name="prenom" value="" placeholder="Prénom"/>
 		</div>
 		<div id="error_inscription_prenom" class="error_formulaire" style="display:none"></div>
-		<div class="loginInput">
+		<div class="formulaireInput">
 			<input class="buttonForm" type="text" name="code" value="" placeholder="Code classe"/>
 		</div>
 		<div id="error_inscription_code" class="error_formulaire"></div>
@@ -33,7 +33,7 @@
 			<div id="error_inscription_general" class="error_formulaire" style="display:none"></div>
 			<div class="login_tools">
 				<div class="inscription_link">
-					<a href="#dev" onclick="hideInsciptionBox();" >connexion</a>
+					<a href="#dev" onclick="hideInscriptionBox();" >connexion</a>
 				</div>
 			</div>
 		</div>
