3b503d7fcfc8e3791dace38d76498e9c76b6b6f6
gsourdrille@gmail.com <gsourdrille@gmail.com@12beb7ba-e8de-a6ef-9cba-bac698d55960>
2014-02-18 11:36:51 +0000

    [DEV] correction anomalie

--- trunk/core/controller/admin_liste_classe_controller.php
+++ trunk/core/controller/admin_liste_classe_controller.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+@session_start();
 require_once ($_SERVER['DOCUMENT_ROOT']."/myschool/core/service/admin_service.php");
 include($_SERVER['DOCUMENT_ROOT']."/myschool/core/controller/commun_controller.php");
 //Maj de la liste des classes
